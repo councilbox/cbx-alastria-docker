a0b443821e5aa3315209c764b30e795d4387d2e2
Rodrigo Martínez <dev@brunneis.com>
2018-06-08 19:56:23 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+NODE_NAME=brunneis
 if [ ! -f ~/alastria/data/IDENTITY ]; then
-    init.sh auto general brunneis
+    init.sh auto general $NODE_NAME
 fi
+init.sh backup general $NODE_NAME
 exec start.sh
