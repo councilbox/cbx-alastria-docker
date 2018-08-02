c45aac96122d1aedf6965de0593748690e1e5650
brunneis <dev@brunneis.com>
2018-08-02 20:49:24 +0200

    Improved backup script: backup keys only option

--- launch.sh
+++ launch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DATA_DIR="$(cat DATA_DIR 2> /dev/null)"
-WORK_DIR="$(pwd)"/alastria-node
+WORK_DIR="$(pwd)"/alastria
 DATA_DIR=${DATA_DIR:-$WORK_DIR}
 
 NODE_NAME="$(cat NODE_NAME 2> /dev/null)"
@@ -9,7 +9,6 @@ NODE_NAME=${NODE_NAME:-REG_UNNAMED_TestNet_2_4_00}
 NODE_TYPE="$(cat NODE_TYPE 2> /dev/null)"
 NODE_TYPE=${NODE_TYPE:-general}
 
-./destroy.sh
 docker run -tid \
 -v $DATA_DIR:/root/alastria \
 -p 21000:21000 \
