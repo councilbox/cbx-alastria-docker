692b6cf643fbadc86ea6fcc684ccf1a334eb8411
brunneis <dev@brunneis.com>
2018-08-06 19:04:17 +0200

    Keep the chain data when updating the container

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,10 @@
 NODE_TYPE=$1
 NODE_NAME=$2
 
-# Backup mode if the identity exists
 if [ ! -f ~/alastria/data/IDENTITY ]; then
     ./init.sh auto $NODE_TYPE $NODE_NAME
 elif [ ! -f ~/alastria/data/DOCKER_VERSION_$DOCKER_VERSION ]; then
-    ./init.sh backup $NODE_TYPE $NODE_NAME \
+    ./update.sh \
     && (rm -f ~/alastria/data/DOCKER_VERSION_* 2> /dev/null; \
         touch ~/alastria/data/DOCKER_VERSION_$DOCKER_VERSION)
 fi
