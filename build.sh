d63ceeafa199f2539df1a594fecb05fa882840c4
Rodrigo Martínez <dev@brunneis.com>
2019-01-23 18:09:30 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 docker build \
 --build-arg DOCKER_VERSION=latest \
---build-arg MONITOR_ENABLED=1 \
--t councilbox/alastria-node:latest-monitor .
-
-docker build \
---build-arg DOCKER_VERSION=latest \
 -t councilbox/alastria-node:latest .
