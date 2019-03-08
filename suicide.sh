d4a7eecf1f19a81496447db2b92cc3d9298d8e65
brunneis <dev@brunneis.com>
2019-03-08 17:05:40 +0100

    fix suicide.sh

--- suicide.sh
+++ suicide.sh
@@ -2,7 +2,7 @@
 sleep 60
 connected=$(curl -s -X POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"web3_clientVersion","params":[],"id":67}' http://localhost:22000)
 while(true); do
-  stuck=$(cat ~/alastria/logs/$(ls -t1 | grep quorum | head -1) | tail -1 | grep "retrieved hash chain is invalid")
+  stuck=$(cat ~/alastria/logs/$(ls -t1 ~/alastria/logs | grep quorum | head -1) | tail -1 | grep "retrieved hash chain is invalid")
   if [ ! $connected -o "$stuck" ]; then
       kill $(ps -aux | awk '{print $2}' | tr " " "\n" | tail -n +2)
   fi
