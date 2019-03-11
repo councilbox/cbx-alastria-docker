983e05bf2ffe2a2c3ce8f8a3691db7cc87225217
Rodrigo Martínez <dev@brunneis.com>
2019-03-11 19:32:05 +0100

    Bugfix, suicide when too many open files

--- suicide.sh
+++ suicide.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+is_stuck () {
+  cat ~/alastria/logs/$(ls -t1 ~/alastria/logs | grep quorum | head -1) | tail -1 | grep "$1"
+}
+
+is_connected () {
+  curl -s -X POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"web3_clientVersion","params":[],"id":67}' http://localhost:22000
+}
+
 sleep 60
-connected=$(curl -s -X POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"web3_clientVersion","params":[],"id":67}' http://localhost:22000)
 while(true); do
-  stuck=$(cat ~/alastria/logs/$(ls -t1 ~/alastria/logs | grep quorum | head -1) | tail -1 | grep "retrieved hash chain is invalid")
-  if [ ! $connected -o "$stuck" ]; then
+  if [ ! $(is_connected) -o "$(is_stuck "retrieved hash chain is invalid")" -o "$(is_stuck "too many open files")" ]; then
       kill $(ps -aux | awk '{print $2}' | tr " " "\n" | tail -n +2)
   fi
+  echo "Loop"
   sleep 5
 done
